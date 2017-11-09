8c58431002ee0dcc4adfa590f4ea4be05a07ef56
Ilya Vladimirskiy <ilya.vladimirskiy@smartfrog.com>
2017-11-09 19:36:21 +0100

    last version of example bot

---
+++ tmp/ivbot/zero_iq_bot_txt.py
@@ -0,0 +1,62 @@
+import random
+
+DICT = {}
+
+
+USERNAME = ''
+filename='./dictionary.txt'
+separator = '|'
+CURRENT_LANGUAGE = 'english'
+
+INTRO_WORDS = ['hello', 'hallo']
+EXIT_WORDS = ['bye', 'tschuess']
+LANGUAGES = ['english', 'german']
+
+is_waiting_for_name = False
+is_leaving = False
+
+def add_question_to_dict(language, question, answer):
+    if language not in DICT:
+        DICT[language] = {}
+    if question not in DICT[language]:
+        DICT[language][question] = [answer]
+    else:
+        DICT[language][question].append(answer)
+
+
+def fill_dict(filename):
+    f = open(filename, 'r')
+    for line in f:
+        data = line.split(separator)
+        language = data[0].lower().rstrip()
+        question = data[1].lower().rstrip()
+        for i in range(2, len(data)):
+            answer = data[i].rstrip()
+            add_question_to_dict(language, question, answer)
+
+
+def get_answer(question):
+    if question in DICT[CURRENT_LANGUAGE]:
+        return random.choice(DICT[CURRENT_LANGUAGE][question]).format(USERNAME)
+    else:
+        return 'do not understand'
+
+
+fill_dict(filename)
+
+while not is_leaving:
+    user_input = raw_input('->').lower().rstrip()
+
+    if is_waiting_for_name:
+        USERNAME = user_input
+        user_input = 'name'
+        is_waiting_for_name = False
+    else:
+        if user_input in INTRO_WORDS:
+            is_waiting_for_name = True
+        elif user_input in LANGUAGES:
+            CURRENT_LANGUAGE = user_input
+        elif user_input in EXIT_WORDS:
+            is_leaving = True
+
+    print(get_answer(user_input))

e0a44e7153af1f5cc01f9d1fba5865627e50df1e
Denis Grafov <denis.grafov@here.com>
2019-01-22 11:50:52 +0100

    Slides 2.0: removed cmake files from sources

--- theory/slides-2-0/remark.js/css/default/CMakeFiles/remark-style-default.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/remark-style-default"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/remark-style-default.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
